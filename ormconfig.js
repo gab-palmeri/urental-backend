50e72660f4f151333d60fe89f2b51a8197d8e5c0
Michele <micheleconti1999@gmail.com>
2021-06-30 18:26:40 +0200

    ormconfig modified

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
    "type": "mysql",
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "database": process.env.DB_NAME,
-   "username": process.env.DB_USER,
-   "password": process.env.DB_PASS,
+   "host": process.env.DB_HOST || "localhost",
+   "port": process.env.DB_PORT || 3306,
+   "database": process.env.DB_NAME || "appwebandmobile",
+   "username": process.env.DB_USER || "root",
+   "password": process.env.DB_PASS || "",
    "synchronize": true,
    "logging": false,
    "entities": [
