a28c306251b46a5cdd7b337e50f83718100a8c41
Gabriele <m.gabriele.palmeri@gmail.com>
2021-07-03 10:29:03 +0200

    Changed VehiclePhoto imgUrl to text

--- src/entity/VehiclePhoto.ts
+++ src/entity/VehiclePhoto.ts
@@ -8,7 +8,7 @@ export class VehiclePhoto {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({type: "varchar", length:32})
+    @Column({type: "text"})
     imgUrl: string;
 
     @ManyToOne(() => Vehicle, vehicle => vehicle.photos)
