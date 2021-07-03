7a243e42da959615a088cbfa2876a46213227828
Gabriele <m.gabriele.palmeri@gmail.com>
2021-07-03 11:00:56 +0200

    Added typeorm migrations support

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,19 @@
 module.exports = {
-   "type": "mysql",
-   "host": process.env.DB_HOST || "localhost",
-   "port": process.env.DB_PORT || 3306,
-   "database": process.env.DB_NAME || "appwebandmobile",
-   "username": process.env.DB_USER || "root",
-   "password": process.env.DB_PASS || "",
-   "synchronize": true,
-   "logging": false,
-   "entities": [
-       "src/entity/*.ts"
-   ],
-   "subscribers": [
-       "src/subscriber/*.ts"
-   ],
-   "migrations": [
-       "src/migration/*.ts"
-   ]
+	"type": "mysql",
+	"host": process.env.DB_HOST || "localhost",
+	"port": process.env.DB_PORT || 3306,
+	"database": process.env.DB_NAME || "appwebandmobile",
+	"username": process.env.DB_USER || "root",
+	"password": process.env.DB_PASS || "",
+	"synchronize": process.env.DB_SYNC || true,
+	"logging": false,
+	"entities": [
+		"src/entity/*.ts"
+	],
+	"migrations": [
+		"src/migration/*.ts"
+	],
+	"cli": {
+		"migrationsDir": "src/migration"
+	}
 }
